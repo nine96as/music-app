2ae77771bc7167c08310310efc301593a98acc1c
nine96as <ashrafsaid79@gmail.com>
2023-12-04 21:15:00 +0000

    Refactor data.js, fix sizing issue with .album

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,45 +1,79 @@
 export const data = [
   {
     id: 1,
-    artist_name: 'Alan Walker',
+    artist_name: 'Travis Scott',
     artist_intro:
-      "Alan Olav Walker, formerly known by his stage name Dj Walkzz, is a British-born Norwegian record producer and DJ. He is best known for his 2015 single 'Faded'.",
+      "Houston-born rapper and producer Travis Scott's psychedelic trap style makes him one of the more distinctive talents to have risen to the height of mainstream commercial popularity. His heavily processed, half-sung/half-rapped style set him apart early on, and he grew from affiliations with Kanye West's GOOD Music and T.I.'s Grand Hustle in the early 2010s to chart-dominating popularity before the close of the decade.",
     artist_genres: [
       {
-        name: 'Electronic'
+        name: 'Hip Hop'
       },
       {
-        name: 'Classical'
+        name: 'Trap'
       }
     ],
     top_songs: [
       {
         cover_art:
-          'https://upload.wikimedia.org/wikipedia/en/d/da/Alan_Walker_-_Faded.png',
-        name: 'Faded',
-        album: 'Different World',
-        length: '3:32',
-        listens: '2,011,799,067'
+          'https://upload.wikimedia.org/wikipedia/en/6/61/Travis_Scott_-_Birds_in_the_Trap_Sing_McKnight.png',
+        name: 'goosebumps',
+        album: 'Birds In The Trap Sing McKnight',
+        length: '4:03',
+        listens: '2,144,209,743'
       },
       {
         cover_art:
-          'https://upload.wikimedia.org/wikipedia/en/d/d7/Alan_Walker_-_Sing_Me_to_Sleep.png',
-        name: 'Sing Me to Sleep',
-        album: 'Different World',
-        length: '3:09',
-        listens: '465,032,129'
+          'https://upload.wikimedia.org/wikipedia/en/0/0b/Astroworld_by_Travis_Scott.jpg',
+        name: 'SICKO MODE',
+        album: 'ASTROWORLD',
+        length: '5:12',
+        listens: '1,985,256,141'
+      },
+      {
+        cover_art:
+          'https://upload.wikimedia.org/wikipedia/en/2/23/Travis_Scott_-_Utopia.png',
+        name: 'I KNOW ?',
+        album: 'UTOPIA',
+        length: '3:31',
+        listens: '250,324,184'
+      },
+      {
+        cover_art:
+          'https://upload.wikimedia.org/wikipedia/en/2/23/Travis_Scott_-_Utopia.png',
+        name: 'MELTDOWN (feat. Drake)',
+        album: 'UTOPIA',
+        length: '4:06',
+        listens: '222,777,062'
+      },
+      {
+        cover_art:
+          'https://upload.wikimedia.org/wikipedia/en/2/23/Travis_Scott_-_Utopia.png',
+        name: 'FE!N (feat. Playboi Carti)',
+        album: 'UTOPIA',
+        length: '3:11',
+        listens: '221,301,999'
       }
     ],
     albums: [
       {
         cover_art:
-          'https://upload.wikimedia.org/wikipedia/en/a/a8/Alan_Walker_%E2%80%93_Different_World.png',
-        name: 'Different World'
+          'https://upload.wikimedia.org/wikipedia/en/2/23/Travis_Scott_-_Utopia.png',
+        name: 'UTOPIA'
       },
       {
         cover_art:
-          'https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/World_of_Walker.webp/316px-World_of_Walker.webp.png',
-        name: 'World of Walker'
+          'https://upload.wikimedia.org/wikipedia/en/0/0b/Astroworld_by_Travis_Scott.jpg',
+        name: 'ASTROWORLD'
+      },
+      {
+        cover_art:
+          'https://upload.wikimedia.org/wikipedia/en/6/61/Travis_Scott_-_Birds_in_the_Trap_Sing_McKnight.png',
+        name: 'Birds In The Trap Sing McKnight'
+      },
+      {
+        cover_art:
+          'https://upload.wikimedia.org/wikipedia/en/0/02/Rodeoalbum.jpg',
+        name: 'Rodeo'
       }
     ]
   }
