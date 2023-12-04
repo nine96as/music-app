c6d293510ede8caa59cc8249dea13c6db655f545
nine96as <nine96@skiff.com>
2023-12-04 20:31:32 +0000

    Add iteration step for outputting components; complete component-erisation

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,18 +1,14 @@
 import { data } from '../assets/data';
+import Album from './Album';
 
 function AlbumsList() {
   return (
     <div className='albums-list-section'>
       <h2>Albums</h2>
       <div className='albums-container'>
-        <div className='album'>
-          <img className='album-cover-art' src={data[0].albums[0].cover_art} />
-          <div className='album-name'>{data[0].albums[0].name}</div>
-        </div>
-        <div className='album'>
-          <img className='album-cover-art' src={data[0].albums[1].cover_art} />
-          <div className='album-name'>{data[0].albums[1].name}</div>
-        </div>
+        {data[0].albums.map((_, i) => (
+          <Album artist_id={0} value={i} key={i} />
+        ))}
       </div>
     </div>
   );
--- src/components/ArtistInfo.jsx
+++ src/components/ArtistInfo.jsx
@@ -1,14 +1,9 @@
-import { data } from '../assets/data';
+import Artist from './Artist';
 
 function ArtistInfo() {
   return (
     <div className='info-section'>
-      <h1 className='artist-name'>{data[0].artist_name}</h1>
-      <p className='artist-intro'>{data[0].artist_intro}</p>
-      <div className='artist-genres'>
-        <div className='genre'>{data[0].artist_genres[0].name}</div>
-        <div className='genre'>{data[0].artist_genres[1].name}</div>
-      </div>
+      <Artist artist_id={0} />
     </div>
   );
 }
