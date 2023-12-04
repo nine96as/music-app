3afe6b033dea60178578bb3119a4be7bcae2bfa8
nine96as <nine96@skiff.com>
2023-12-04 21:15:00 +0000

    Refactor data.js, fix sizing issue with .album

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { data } from '../assets/data';
+import Like from './Like';
 
 function Song({ artist_id, value }) {
   return (
@@ -17,24 +18,7 @@ function Song({ artist_id, value }) {
       <div className='song-listens'>
         {data[artist_id].top_songs[value].listens}
       </div>
-      <button className='like-btn'>
-        <svg
-          fill='none'
-          height='24'
-          shapeRendering='geometricPrecision'
-          stroke='currentColor'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          strokeWidth='1.5'
-          viewBox='0 0 24 24'
-          width='24'
-        >
-          <path
-            d='M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z'
-            fill='var(--geist-fill)'
-          />
-        </svg>
-      </button>
+      <Like />
     </div>
   );
 }
