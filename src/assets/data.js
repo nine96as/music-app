9974672d65c36cfc635e6963ad2983e6e3b18876
nine96as <ashrafsaid79@gmail.com>
2023-12-04 19:08:58 +0000

    Create data file for artist

---
+++ src/assets/data.js
@@ -0,0 +1,54 @@
+export const data = [
+  {
+    id: 1,
+    artist_name: 'Alan Walker',
+    artist_intro:
+      "Alan Olav Walker, formerly known by his stage name Dj Walkzz, is a British-born Norwegian record producer and DJ. He is best known for his 2015 single 'Faded'.",
+    artist_genres: [
+      {
+        name: 'Electronic'
+      },
+      {
+        name: 'Classical'
+      }
+    ],
+    top_songs: [
+      {
+        cover_art:
+          'https://upload.wikimedia.org/wikipedia/en/d/da/Alan_Walker_-_Faded.png',
+        name: 'Faded',
+        album: 'Different World',
+        length: '3:32',
+        listens: '2,011,799,067'
+      },
+      {
+        cover_art:
+          'https://upload.wikimedia.org/wikipedia/en/d/da/Alan_Walker_-_Faded.png',
+        name: 'Faded',
+        album: 'Different World',
+        length: '3:32',
+        listens: '2,011,799,067'
+      },
+      {
+        cover_art:
+          'https://upload.wikimedia.org/wikipedia/en/d/da/Alan_Walker_-_Faded.png',
+        name: 'Faded',
+        album: 'Different World',
+        length: '3:32',
+        listens: '2,011,799,067'
+      }
+    ],
+    albums: [
+      {
+        cover_art:
+          'https://upload.wikimedia.org/wikipedia/en/a/a8/Alan_Walker_%E2%80%93_Different_World.png',
+        name: 'Different World'
+      },
+      {
+        cover_art:
+          'https://upload.wikimedia.org/wikipedia/en/a/a8/Alan_Walker_%E2%80%93_Different_World.png',
+        name: 'Different World'
+      }
+    ]
+  }
+];
