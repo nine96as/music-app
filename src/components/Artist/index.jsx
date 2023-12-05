238734cbe623619c6309c2933360814fb1fbca67
nine96as <nine96@skiff.com>
2023-12-05 20:17:53 +0000

    Revamp Artist, Genre cmpts to use api calls, add useEffect and helper fetch funcs

--- src/components/Artist/index.jsx
+++ src/components/Artist/index.jsx
@@ -1,15 +1,58 @@
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 import { data } from '../../assets/data';
 import Genre from '../Genre';
 
 function Artist({ artist_id }) {
+  const [name, setName] = useState('');
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    getArtistName();
+    getArtistGenres();
+  }, []);
+
+  const getArtistName = async () => {
+    try {
+      const url = `https://api.musixmatch.com/ws/1.1/artist.get?artist_mbid=e4a51f17-a57b-47b1-b37b-f552d0f8e9e6&apikey=${
+        import.meta.env.VITE_API_KEY
+      }`;
+      const resp = await fetch(url);
+      const data = await resp.json();
+
+      setName(data.message.body.artist.artist_name);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getArtistGenres = async () => {
+    const validGenreIds = [1073, 1068];
+
+    try {
+      const url = `https://api.musixmatch.com/ws/1.1/music.genres.get?apikey=${
+        import.meta.env.VITE_API_KEY
+      }`;
+      const resp = await fetch(url);
+      const data = await resp.json();
+      const genreArr = data.message.body.music_genre_list;
+      const filteredArr = genreArr.filter((g) =>
+        validGenreIds.includes(g.music_genre.music_genre_id)
+      );
+
+      setGenres(filteredArr);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <>
-      <h1 className='artist-name'>{data[artist_id].artist_name}</h1>
+      <h1 className='artist-name'>{name}</h1>
       <p className='artist-intro'>{data[artist_id].artist_intro}</p>
       <div className='artist-genres'>
-        {data[0].artist_genres.map((_, i) => (
-          <Genre artist_id={0} value={i} key={i} />
+        {genres.map((g, i) => (
+          <Genre value={g.music_genre.music_genre_name} key={i} />
         ))}
       </div>
     </>
--- src/components/Genre/index.jsx
+++ src/components/Genre/index.jsx
@@ -1,15 +1,11 @@
 import PropTypes from 'prop-types';
-import { data } from '../../assets/data';
 
-function Genre({ artist_id, value }) {
-  return (
-    <div className='genre'>{data[artist_id].artist_genres[value].name}</div>
-  );
+function Genre({ value }) {
+  return <div className='genre'>{value}</div>;
 }
 
 Genre.propTypes = {
-  artist_id: PropTypes.number,
-  value: PropTypes.number
+  value: PropTypes.string
 };
 
 export default Genre;
