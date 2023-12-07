fb26f7fc29d589171c616cad315a35fcff6fc744
nine96as <ashrafsaid79@gmail.com>
2023-12-07 12:36:13 +0000

    Implement dynamic fetching of getSongs, add details state

--- src/components/SongList/index.jsx
+++ src/components/SongList/index.jsx
@@ -5,23 +5,29 @@ import Song from '../Song';
 
 function SongList({ artist_id }) {
   const [songs, setSongs] = useState([]);
-  const [coverArt, setCoverArt] = useState('');
+  const [details, setDetails] = useState([]);
 
   useEffect(() => {
-    getAlbumCoverArt();
+    getAlbumDetails();
     getSongs();
   }, []);
 
-  const getAlbumCoverArt = async () => {
+  const getAlbumDetails = async () => {
     const resp = await axios.get(
       `https://theaudiodb.com/api/v1/json/2/album.php?i=${artist_id}`
     );
-    setCoverArt(resp.data.album.pop().strAlbumThumb);
+    const album = resp.data.album.pop();
+    setDetails([
+      {
+        album_id: parseInt(album.idAlbum),
+        cover_art: album.strAlbumThumb
+      }
+    ]);
   };
 
   const getSongs = async () => {
     const resp = await axios.get(
-      'https://theaudiodb.com/api/v1/json/2/track.php?m=2408685'
+      `https://theaudiodb.com/api/v1/json/2/track.php?m=${details[0].album_id}`
     );
     const songs = resp.data.track;
     setSongs(songs);
@@ -33,7 +39,7 @@ function SongList({ artist_id }) {
       <div className='song-container table-container'>
         {songs.map((s, i) => (
           <Song
-            coverArt={coverArt}
+            coverArt={details[0].cover_art}
             name={s.strTrack}
             album={s.strAlbum}
             duration={s.intDuration}
