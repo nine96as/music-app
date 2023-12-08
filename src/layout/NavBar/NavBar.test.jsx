6b4665ad035a778d2e3a7a39c95d73895f7e6c09
nine96as <nine96@skiff.com>
2023-12-08 17:34:50 +0000

    Implement test cases for NavBar

---
+++ src/layout/NavBar/NavBar.test.jsx
@@ -0,0 +1,28 @@
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { describe, expect, it } from 'vitest';
+import NavBar from '.';
+
+expect.extend(matchers);
+
+describe('NavBar component', () => {
+  render(
+    <MemoryRouter>
+      <NavBar />
+    </MemoryRouter>
+  );
+
+  it('displays a NavBar with 2 children', () => {
+    const nav = screen.getByRole('navigation');
+
+    expect(nav.childNodes.length).toBe(2);
+  });
+
+  it('displays NavBar links', () => {
+    const links = screen.getAllByRole('link');
+
+    expect(links[0].childNodes[0].textContent).toBe('Music');
+    expect(links[1].childNodes[0].textContent).toBe('Artists');
+  });
+});
